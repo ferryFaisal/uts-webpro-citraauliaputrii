ec6063da22526154105d88bb177b707579e0dfd1
citraauliaputrii <115518583+citraauliaputrii@users.noreply.github.com>
2022-10-28 16:30:02 +0700

    Create delete_data.php

---
+++ delete_data.php
@@ -0,0 +1,14 @@
+<?php
+require 'database.php';
+
+$id = $_GET['id'];
+$sql = "DELETE FROM products WHERE id = '$id'";
+
+if ($conn->query($sql) === TRUE) {
+    // echo "Record deleted successfully";
+    header('location: tabel.php');
+} else {
+    echo "Error deleting record: " . $conn->error;
+}
+
+$conn->close();
